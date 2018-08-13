39502bdade9af029b6feeeca5375befd3741e553
Spencer Neveux <33857808+spencerneveux@users.noreply.github.com>
2018-08-13 13:01:09 -0700

    Create noX.java

---
+++ Recursion-1/noX.java
@@ -0,0 +1,11 @@
+public String noX(String str) {
+  if (str.length() < 1) {
+    return str;
+  }
+  
+  if (str.charAt(0) == 'x') {
+    return noX(str.substring(1));
+  }
+  
+  return str.charAt(0) + noX(str.substring(1));
+}
