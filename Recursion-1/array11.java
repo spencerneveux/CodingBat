4dad71be3952db8f8857fb3449176a03336eab20
Spencer Neveux <33857808+spencerneveux@users.noreply.github.com>
2018-08-13 13:08:07 -0700

    Create array11.java

---
+++ Recursion-1/array11.java
@@ -0,0 +1,13 @@
+public int array11(int[] nums, int index) {
+  int count = 0;
+  if (index >= nums.length) {
+    return count;
+  }
+  
+  if (nums[index] == 11) {
+    count++;
+    return count + array11(nums, index + 1);
+  }
+  
+  return count + array11(nums, index + 1);
+}
