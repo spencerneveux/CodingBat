2aecc7bd3a9a45b89789e73321c94f936695bbfc
Spencer Neveux <33857808+spencerneveux@users.noreply.github.com>
2018-08-13 12:57:21 -0700

    Create changeXY.java

---
+++ Recursion-1/changeXY.java
@@ -0,0 +1,11 @@
+public String changeXY(String str) {
+  if (str.length() < 1) {
+    return str;
+  }
+  
+  if (str.charAt(0) == 'x') {
+    return "y" + changeXY(str.substring(1));
+  }
+  
+  return str.charAt(0) + changeXY(str.substring(1));
+}
